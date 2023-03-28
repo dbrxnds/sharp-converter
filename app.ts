e0a3725b1687342513d366a04444a3a7a3c62bb0
David Brands <me@dbrands.dev>
2023-03-28 17:54:05 +0200

    [-] Init

--- app.ts
+++ app.ts
@@ -1,59 +1,84 @@
 import express from "express";
+import sharp from "sharp";
+import { z } from "zod";
+import fetch from "node-fetch";
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+const QueryParams = z.object({
+  url: z.string().url(),
+  quality: z.coerce.number().min(1).max(100).optional(),
+  outputType: z.enum(["webp", "png", "jpeg"]),
+  width: z.coerce.number().min(1).optional(),
+  height: z.coerce.number().min(1).optional(),
+  maxFileSize: z.coerce.number().min(1000).optional(),
+});
+
+async function reduceImageQuality(
+  inputBuffer: ArrayBuffer,
+  maxSize: number,
+  outputType: z.infer<typeof QueryParams>["outputType"]
+): Promise<Buffer> {
+  let quality = 100;
+  let step = 5;
+  let outputBuffer: Buffer;
+  let currentSize: number;
+
+  do {
+    outputBuffer = await sharp(inputBuffer)
+      [outputType]({ quality: quality })
+      .toBuffer();
+
+    currentSize = outputBuffer.length;
+    quality -= step;
+  } while (currentSize > maxSize && quality > 0);
+
+  if (quality <= 0) {
+    throw new Error(
+      "Unable to reduce the image size to the specified max file size."
+    );
+  }
+
+  return outputBuffer;
+}
+
+app.get("/", async (req, res) => {
+  const parsedQuery = QueryParams.safeParse(req.query);
+
+  if (!parsedQuery.success) {
+    res.status(400).send(parsedQuery.error.toString());
+    return;
+  }
+  const queryParams = parsedQuery.data;
+
+  const imageBuffer = await fetch(queryParams.url).then((res) =>
+    res.arrayBuffer()
+  );
+  const image = sharp(imageBuffer);
+
+  if (queryParams.quality) {
+    image[queryParams.outputType]({ quality: queryParams.quality });
+  }
+
+  if (queryParams.width) {
+    image.resize(queryParams.width, queryParams.height);
+  }
+
+  try {
+    const constrainedImage = queryParams.maxFileSize
+      ? await reduceImageQuality(
+          imageBuffer,
+          queryParams.maxFileSize,
+          queryParams.outputType
+        )
+      : await image.toBuffer();
+
+    res.contentType(`image/${queryParams.outputType}`);
+    res.send(constrainedImage);
+  } catch (error) {
+    res.status(500).send(error?.message || "Unknown error");
+  }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
